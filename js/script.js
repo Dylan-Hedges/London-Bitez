c9d531f3a6d748c94df91d57f309c59659d07711
Dylan-Hedges <d.hedges1@hotmail.co.uk>
2017-07-28 07:14:18 +0100

    Added top picks page, added google maps page (included gmaps.js file and updated script.js file)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
 
+    //Fade in functionality - uses Animate.css
         $('.js--wp-1').waypoint(function(direction) {
             $('.js--wp-1').addClass('animated fadeIn');
         }, {
             offset: '90%'
         });
+
+    //Google maps starting position
+        new GMaps({
+        div: '.map',
+        lat: 51.5088700,
+        lng: -0.1273014
+    });
 
 });
