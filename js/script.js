e7423a0696d84b25f66dd553b1a43afe622c01d3
Dylan-Hedges <d.hedges1@hotmail.co.uk>
2017-08-21 11:20:10 +0100

    Merged with home branch (added links/instafeed/scrolling/animations), added scrollling to footer

--- js/script.js
+++ js/script.js
@@ -7,11 +7,54 @@ $(document).ready(function() {
             offset: '90%'
         });
 
+        $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeIn');
+        }, {
+            offset: '90%'
+        });
+
+        $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated jackInTheBox');
+        }, {
+            offset: '80%'
+        });
+
+        $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated fadeInUp');
+        }, {
+            offset: '70%'
+        });
+
+
+
+
     //Google maps starting position
         new GMaps({
         div: '.map',
         lat: 51.5088700,
         lng: -0.1273014
     });
+
+
+      var scrollLink = $('.scroll');
+
+      // Smooth scrolling
+      scrollLink.click(function(e) {
+        e.preventDefault();
+        $('body,html').animate({
+          scrollTop: $(this.hash).offset().top
+        }, 1000 );
+      });
+
+
+
+
+         var userFeed = new Instafeed({
+            get: 'user',
+            userId: '',
+            accessToken: '',
+            resolution: 'standard_resolution'
+        });
+        userFeed.run();
 
 });
