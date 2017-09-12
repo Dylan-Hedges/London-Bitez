14b0bba4e91687feac95e10123ec70bbffa3f26d
Dylan-Hedges <d.hedges1@hotmail.co.uk>
2017-09-12 10:43:10 +0100

    Fix for nav bar text, updated content and images/animations for locations and top picks

--- js/script.js
+++ js/script.js
@@ -20,9 +20,14 @@ $(document).ready(function() {
         });
 
         $('.js--wp-4').waypoint(function(direction) {
-            $('.js--wp-4').addClass('animated fadeInUp');
+            $('.js--wp-4').addClass('animated bounceIn');
         }, {
             offset: '70%'
+        });
+        $('.js--wp-5').waypoint(function(direction) {
+            $('.js--wp-5').addClass('animated bounceIn');
+        }, {
+            offset: '80%'
         });
 
 //---------------------Scrolling---------------------
@@ -58,18 +63,66 @@ function initMap() {
     //Starting position for map
     var startPosition = {lat: 51.511196, lng: -0.126274};
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
+        zoom: 13,
         center: startPosition
     });
     //Array of objects containing marker coordinates
     var markers = [
         {
-            coords:{lat: 51.511196, lng: -0.126274},
-            content:'<p>Heading 1</p>'
+            coords:{lat: 51.5132264, lng: -0.136422},
+            content:'<p><b>BAO London</b><p>Intimate, wood-furnished space for steamed milk buns, </p><p>other inventive Taiwanese eats & drinks.</p>'
         },
         {
             coords:{lat: 51.511196,lng: -0.226274},
-            content:'<p>Heading 2</p>'
+            content:'<p><b>Hoppers</b><p>Family-style Sri Lankan cooking and inventive cocktails</p><p>offered in vibrant, relaxed surroundings.</p>'
+        },
+        {
+            coords:{lat: 51.513642,lng: -0.128350},
+            content:'<p><b>Chick n sours</b><p>Contemporary take on a classic takeout offering sour</p><p>cocktails and fried chicken dishes.</p>'
+        },
+        {
+            coords:{lat: 51.513619,lng: -0.134897},
+            content:'<p><b>Mediterranean cafe</b><p>Mediterranean food served in intimate candlelit spaces</p><p>draped in curtains plus theatre posters.</p>'
+        },
+        {
+            coords:{lat: 51.511425,lng: -0.121815},
+            content:'<p><b>MEATmarket</b><p>Quirkily named burgers and hot dogs plus Southern-style sides</p><p>and whisky milkshakes. Few seats.</p>'
+        },
+        {
+            coords:{lat: 51.513789,lng: -0.134910},
+            content:'<p><b>Reddog saloon</b><p>All-day Vietnamese cafe for a variety of spring rolls and other</p><p>for bbq and meat dishes.</p>'
+        },
+        {
+            coords:{lat: 51.508127,lng: -0.124037},
+            content:'<p><b>Moc Kitchen</b><p>All-day Vietnamese cafe for a variety of spring rolls and other</p><p>health-focused fare in casual digs.</p>'
+        },
+        {
+            coords:{lat: 51.513970,lng: -0.125602},
+            content:'<p><b>Flesh and buns</b><p>Izakaya pub-style venue with a rock soundtrack,</p><p>serving steamed buns with meat or seafood fillings.</p>'
+        },
+        {
+            coords:{lat: 51.514637,lng: -0.133524},
+            content:'<p><b>Billy and the chicks</b><p>Fried chicken with house spices, served with cocktails</p><p>& craft beers in a hip nook with DJ tunes.</p>'
+        },
+        {
+            coords:{lat: 51.537279,lng: -0.080837},
+            content:'<p><b>The barge house</b><p>Traditional food with a Latin twist is on the menu of this airy,</p><p>bleached wood canalside bar/diner.</p>'
+        },
+        {
+            coords:{lat: 51.444487,lng: -0.150825},
+            content:'<p><b>Milk</b><p>Daytime cafe with artisan speciality coffees and innovative cuisine</p><p>at breakfast and lunch.</p>'
+        },
+        {
+            coords:{lat: 51.505487,lng: -0.090734},
+            content:'<p><b>Borough Market</b><p>food market heaven offering all the food you could dream of</p>'
+        },
+        {
+            coords:{lat: 51.512671,lng: -0.138965},
+            content:'<p><b>Le Bab</b><p>Casual Middle Eastern & Asian spot for creative shawarma & other</p><p>fare cooked over charcoal & wood.</p>'
+        },
+        {
+            coords:{lat: 51.512547,lng: -0.138859},
+            content:'<p><b>Stax diner</b><p>Gourmet burgers and popcorn shrimp in small, </p><p>first-floor, American-run cafe with food-tin lighting.</p>'
         }
         ];
 
